e3c7f22f001f4fb8a990892352c708a54f88f51c
mike groffen <mikegroffen4@gmail.com>
2019-11-05 00:09:52 +0100

    unit test added

--- final project/final project/Sales.cs
+++ final project/final project/Sales.cs
@@ -1,61 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace final_project
 {
+    public class sales
+    {
+        public float TotalPricecalc(List<SaleLinesItem> productendb1)
+        {
+            float totaalbedrag = 0;
+            bool verzendingskosten = false;
+            foreach (SaleLinesItem product in productendb1)
+            {
+                if (product.producttype == "Fysiek") { verzendingskosten = true; }
+                totaalbedrag += product.quantitypricecalc();
+            }
+
+            // als het een fysiekproduct bevat voegen we verzendingskosten van 2,50 bij.
+            if (verzendingskosten)
+                return totaalbedrag + 2.50f;
+            else return totaalbedrag;
+        }
+    }
+
     public partial class Sales : Form
     {
         private Betaling _betaling;
         List<SaleLinesItem> productendb;
+        sales s = new sales();
 
         public Sales(List<SaleLinesItem> productendb1)
         {
             InitializeComponent();
             productendb = productendb1;
         }
-
-        private float TotalPricecalc(List<SaleLinesItem> productendb1)
-        {
-            float totaalbedrag = 0;
-            bool verzendingskosten = false;
-            foreach(SaleLinesItem product in productendb1)
-            {
-                if (product.producttype == "Fysiek") { verzendingskosten = true; }
-               totaalbedrag += product.quantitypricecalc();
-            }
         
-            // als het een fysiekproduct bevat voegen we verzendingskosten van 2,50 bij.
-            if (verzendingskosten)
-                return totaalbedrag + 2.50f;
-            else return totaalbedrag;
-        }
-
         //de paypal knop
         private void button1_Click(object sender, EventArgs e)
         {
             _betaling = new Paypal();
-            _betaling.Betaalmethode(TotalPricecalc(productendb),productendb);
+            _betaling.Betaalmethode(s.TotalPricecalc(productendb),productendb);
         }
 
         //de pinpas knop
         private void button2_Click(object sender, EventArgs e)
         {
             _betaling = new Pinpas();
-            _betaling.Betaalmethode(TotalPricecalc(productendb),productendb);
+            _betaling.Betaalmethode(s.TotalPricecalc(productendb),productendb);
         }
 
         //de creditcard knop
         private void button3_Click(object sender, EventArgs e)
         {
             _betaling = new CreditCard();
-            _betaling.Betaalmethode(TotalPricecalc(productendb),productendb);
+            _betaling.Betaalmethode(s.TotalPricecalc(productendb),productendb);
         }
 
         //terug knop
--- final project/unittestshipping.cs/UnitTest1.cs
+++ final project/unittestshipping.cs/UnitTest1.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using final_project;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace unittestshipping.cs
 {
@@ -7,9 +9,33 @@ namespace unittestshipping.cs
     public class UnitTest1
     {
         [TestMethod]
-        public void TestShippingCost()
+        public void TestShippingCostgeen()
         {
-            //Sales sale = new Sales();
+            float expected = 10.00f;
+
+            List<SaleLinesItem> productdb = new List<SaleLinesItem>();
+            ProductInformatie product1 = new ProductInformatie("testitem1", "testbeschrijving1", 10.00f, "Digitaal", "");
+            productdb.Add(new SaleLinesItem("1", product1));
+
+            sales sale = new sales();
+            float actual = sale.TotalPricecalc(productdb);
+
+            Assert.AreEqual(expected, actual, 0.001, "Verzendkosten zijn erbij toegevoegd. test failed");
+        }
+
+        [TestMethod]
+        public void TestShippingCostmet()
+        {
+            float expected = 12.50f;
+
+            List<SaleLinesItem> productdb = new List<SaleLinesItem>();
+            ProductInformatie product1 = new ProductInformatie("testitem1", "testbeschrijving1", 10.00f, "Fysiek", "");
+            productdb.Add(new SaleLinesItem("1", product1));
+
+            sales sale = new sales();
+            float actual = sale.TotalPricecalc(productdb);
+
+            Assert.AreEqual(expected, actual, 0.001, "Verzendkosten zijn er niet bij toegevoegd. test failed");
         }
     }
 }
